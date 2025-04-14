10f7eb065f2f48572821a3183dd434b8a3d6c37d
Amoghk04 <kalasapuraamogh@gmail.com>
2025-04-14 18:23:10 +0530

    escapeverse: Unit Tests #3

--- escapeverse/jest.config.cjs
+++ escapeverse/jest.config.cjs
@@ -4,7 +4,7 @@
 
 module.exports = {
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.cjs"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "^@/(.*)$": "<rootDir>/src/$1"
@@ -13,5 +13,7 @@ module.exports = {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
   },
   coveragePathIgnorePatterns: ["/node_modules/", "/tests/"],
-  testMatch: ["**/__tests__/**/*.js?(x)", "**/?(*.)+(spec|test).js?(x)"]
+  testMatch: ["**/__tests__/**/*.js?(x)", "**/?(*.)+(spec|test).js?(x)"],
+  // Add this to exclude Playwright tests
+  modulePathIgnorePatterns: ["<rootDir>/tests/", ".*\\.spec\\.js$"],
 };
--- escapeverse/jest.setup.cjs
+++ escapeverse/jest.setup.cjs
@@ -3,6 +3,9 @@
 /* eslint-disable no-undef */
 
 /* eslint-env jest */
+const { TextEncoder, TextDecoder } = require('util');
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 require('@testing-library/jest-dom');
 const { toHaveNoViolations } = require('jest-axe');
